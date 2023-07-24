2eb9c22c33d956333ebf8fe4f306f08c93a80c58
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2023-07-24 10:49:40 +0200

    model tweak + added test files

--- vis_neural_net.py
+++ vis_neural_net.py
@@ -1,9 +1,12 @@
+import keras.losses
 import tensorflow as tf
 from keras import layers
 from keras import models
 from keras.datasets import mnist
 from keras.preprocessing.image import img_to_array, load_img
 import numpy as np
+import os
+import pathlib
 
 
 def train_model():
@@ -20,9 +23,10 @@ def train_model():
     model.add(layers.Flatten(input_shape=(28, 28)))
     model.add(layers.Dense(256, activation='relu'))
     model.add(layers.Dense(10, activation='softmax'))
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=8, batch_size=64, validation_data=(x_test, y_test))
+    model.compile(optimizer=keras.optimizers.Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=30, batch_size=64, verbose=2, validation_data=(x_test, y_test))
 
     model.save('model.keras')
 
@@ -30,9 +34,9 @@ def train_model():
 def val(img_path):
     loaded_model = tf.keras.models.load_model('model.keras')
 
-    img_path = img_path
     image = load_img(img_path, color_mode='grayscale', target_size=(28, 28))
-    input_arr = (np.expand_dims(img_to_array(image), axis=0)).astype('float32') / 255.0
+    input_arr = img_to_array(image).astype('float32') / 255.0
+    input_arr = np.reshape(input_arr, (1, 28, 28))
     predictions = loaded_model.predict(input_arr)
     predicted_class = np.argmax(predictions)
 
@@ -40,13 +44,8 @@ def val(img_path):
 
 
 def main():
-    #train_model()
-    val("test_images/3.png")
-    val("test_images/1.png")
-    val("test_images/5.png")
-    val("test_images/7.png")
-    val("test_images/8.png")
-    val("test_images/9.png")
+    for file in os.listdir(os.path.join(pathlib.Path(__file__).parent, 'test_images')):
+        val(os.path.join(pathlib.Path(__file__).parent, 'test_images', file))
 
 
 if __name__ == '__main__':
